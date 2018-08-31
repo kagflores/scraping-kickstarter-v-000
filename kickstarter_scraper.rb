bad30cb80b2825f5ef1e39b0f4d4b08e8b09b565
Karen Flores <karen.ag.flores@gmail.com>
2018-08-31 18:50:34 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -15,5 +15,5 @@ end
 #title: project.css("h2.bbcard_name strong a").text
 #image link: project.css("div.project-thumbnail a img").attribute("src").value
 #description: project.css("p.bbcard_blurb").text
-#location: 
+#location: project.css("ul.project-meta a span.location-name").text
 create_project_hash
